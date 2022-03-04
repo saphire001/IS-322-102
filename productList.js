110377931129006ef890076c28a99a5a8d5ca313
saanikajoshi <spj29@njit.edu>
2022-03-04 15:30:23 -0500

    images to table

--- productList.js
+++ productList.js
@@ -5,16 +5,16 @@ var Ran = 'Ran';
 	console.log(window.Ran); 
 
 	const mockDatabase = [
-		{ _id: '2500', name: 'Love Yourself: Tear', published: true},
-		{ _id: '2000', name: 'The Chaos Chapter: Freeze', published: true},
-		{ _id: '2500', name: 'BTS, The Best', published: false},
-		{ _id: '2000', name: 'The Dream Chapter: Eterinty', published: false},
-		{ _id: '2500', name: 'Wings', published: true},
-		{ _id: '1500', name: 'The War', published: false},
-		{ _id: '1500', name: 'Obsession', published: true},
-		{ _id: '1500', name: 'XOXO', published: false},
-		{ _id: '1000', name: 'In Life', published: true},
-		{ _id: '1000', name: 'No Easy', published: false},
+		{ _id: '2500', name: 'Love Yourself: Tear', published: true, srcc:"Images/LYT.jpeg"},
+		{ _id: '2000', name: 'The Chaos Chapter: Freeze', published: true,  srcc: "Images/TCCF.png"},
+		{ _id: '2500', name: 'BTS, The Best', published: false, srcc:"Images/BTB.jpeg"},
+		{ _id: '2000', name: 'The Dream Chapter: Eterinty', published: false, srcc:"Images/TDCE.jpeg"},
+		{ _id: '2500', name: 'Wings', published: true, srcc: "Images/W.png"},
+		{ _id: '1500', name: 'The War', published: false, srcc:"Images/TW.jpeg"},
+		{ _id: '1500', name: 'Obsession', published: true, srcc:"Images/O.png"},
+		{ _id: '1500', name: 'XOXO', published: false, srcc:"Images/XOXO.jpeg"},
+		{ _id: '1000', name: 'In Life', published: true, srcc:"Images/IL.jpeg"},
+		{ _id: '1000', name: 'No Easy', published: false, srcc:"Images/NE.jpeg"},
 	];
 
 	function renderList (results) {
@@ -22,7 +22,8 @@ var Ran = 'Ran';
 
 		tB.innerHTML = '';
 		var tR = results.map(function (result, index) {
-			return '<tr><td>' + result.name +'</td></tr>';
+			return '<tr><td>' + result.name + '<img src= "' +result.srcc +
+			'"alt="XOXO" style="width:20%"> ' + '</td></tr>';
 		});
 		tR.forEach(function (row) {
 			tB.innerHTML += row; 
